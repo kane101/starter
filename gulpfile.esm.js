54f90325b117a88be11edbab92cc127b1f0b5801
Kane Ejoh <kane@outlook.co.uk>
2021-08-22 20:32:45 +0100

    init commit

---
+++ gulpfile.esm.js
@@ -0,0 +1,39 @@
+import { src, dest, watch, series } from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import cssnano from 'cssnano';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+import browserSync from 'browser-sync';
+import rename from 'gulp-rename';
+const sync = browserSync.create();
+const sass = gulpSass(dartSass);
+
+function scssTask() {
+    return src('assets/scss/main.scss', { sourcemaps: true })
+        .pipe(sass())
+        .pipe(postcss([autoprefixer, cssnano]))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('assets/css', { sourcemaps: '.' }));
+}
+
+function BrowserSync(cb) {
+    sync.init({
+        server: {
+            baseDir: './',
+        },
+    });
+    cb();
+}
+
+function browserSyncReload(cb) {
+    sync.reload();
+    cb();
+}
+
+function watchTask() {
+    watch('*.html', browserSyncReload);
+    watch(['assets/scss/**/*.scss'], series(scssTask, browserSyncReload));
+}
+
+exports.default = series(scssTask, BrowserSync, watchTask);
